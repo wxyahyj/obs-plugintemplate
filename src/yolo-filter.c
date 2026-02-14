982ed3d65b8a66312cc7e8f873b180729a444938
Pluto <3423377239@qq.com>
2026-02-15 00:48:06 +0800

    324234

---
+++ src/yolo-filter.c
@@ -0,0 +1,73 @@
+
+#include &lt;obs-module.h&gt;
+#include &lt;stdlib.h&gt;
+
+struct my_filter_data {
+	obs_source_t *source;
+};
+
+static const char *my_filter_name(void *unused)
+{
+	UNUSED_PARAMETER(unused);
+	return "YOLO 识别";
+}
+
+static void *my_filter_create(obs_data_t *settings, obs_source_t *source)
+{
+	struct my_filter_data *filter = (struct my_filter_data *)bzalloc(sizeof(struct my_filter_data));
+	filter-&gt;source = source;
+
+	blog(LOG_INFO, "[YOLO] 创建成功！");
+
+	obs_source_update(source, settings);
+	return filter;
+}
+
+static void my_filter_destroy(void *data)
+{
+	struct my_filter_data *filter = (struct my_filter_data *)data;
+	if (filter)
+		bfree(filter);
+}
+
+static void my_filter_defaults(obs_data_t *settings)
+{
+	UNUSED_PARAMETER(settings);
+}
+
+static obs_properties_t *my_filter_properties(void *data)
+{
+	UNUSED_PARAMETER(data);
+
+	obs_properties_t *props = obs_properties_create();
+	obs_properties_add_text(props, "info", "纯C最小工作版本", OBS_TEXT_INFO);
+	return props;
+}
+
+static void my_filter_update(void *data, obs_data_t *settings)
+{
+	UNUSED_PARAMETER(data);
+	UNUSED_PARAMETER(settings);
+}
+
+static void my_filter_video_render(void *data, gs_effect_t *effect)
+{
+	struct my_filter_data *filter = (struct my_filter_data *)data;
+
+	if (obs_source_process_filter_begin(filter-&gt;source, GS_RGBA, OBS_ALLOW_DIRECT_RENDERING)) {
+		obs_source_process_filter_end(filter-&gt;source, effect, 0, 0);
+	}
+}
+
+struct obs_source_info yolo_filter_info = {
+	.id             = "yolo_recognizer_filter",
+	.type           = OBS_SOURCE_TYPE_FILTER,
+	.output_flags   = OBS_SOURCE_VIDEO,
+	.get_name       = my_filter_name,
+	.create         = my_filter_create,
+	.destroy        = my_filter_destroy,
+	.get_defaults   = my_filter_defaults,
+	.get_properties = my_filter_properties,
+	.update         = my_filter_update,
+	.video_render   = my_filter_video_render,
+};
--- src/yolo-filter.h
+++ src/yolo-filter.h
@@ -1,11 +1,12 @@
+
 #ifndef YOLO_FILTER_H
 #define YOLO_FILTER_H
+
+#include &lt;obs-module.h&gt;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#include <obs-module.h>
 
 extern struct obs_source_info yolo_filter_info;
 

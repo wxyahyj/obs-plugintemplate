3b8bfbcd36f54c035650b7a3c5e8d4bd79eafb72
Pluto <3423377239@qq.com>
2026-02-15 01:38:15 +0800

    21312

--- src/yolo-filter.c
+++ src/yolo-filter.c
@@ -1,72 +1,80 @@
-#include <obs-module.h>
-#include <stdlib.h>
+#include <obs-module.h> 
+#include <stdlib.h> 
 
-struct my_filter_data {
-	obs_source_t *source;
-};
+struct my_filter_data { 
+	obs_source_t *source; 
+}; 
 
-static const char *my_filter_name(void *unused)
-{
-	UNUSED_PARAMETER(unused);
-	return "YOLO 识别";
-}
+static const char *my_filter_name(void *unused) 
+{ 
+	UNUSED_PARAMETER(unused); 
+	return "YOLO 识别"; 
+} 
 
-static void *my_filter_create(obs_data_t *settings, obs_source_t *source)
-{
-	struct my_filter_data *filter = (struct my_filter_data *)bzalloc(sizeof(struct my_filter_data));
-	filter->source = source;
+static void *my_filter_create(obs_data_t *settings, obs_source_t *source) 
+{ 
+	struct my_filter_data *filter = (struct my_filter_data *)bzalloc(sizeof(struct my_filter_data)); 
+	filter->source = source; 
 
-	blog(LOG_INFO, "[YOLO] 创建成功！");
+	blog(LOG_INFO, "[YOLO] 创建成功！"); 
 
-	obs_source_update(source, settings);
-	return filter;
-}
+	obs_source_update(source, settings); 
+	return filter; 
+} 
 
-static void my_filter_destroy(void *data)
-{
-	struct my_filter_data *filter = (struct my_filter_data *)data;
-	if (filter)
-		bfree(filter);
-}
+static void my_filter_destroy(void *data) 
+{ 
+	struct my_filter_data *filter = (struct my_filter_data *)data; 
+	if (filter) 
+		bfree(filter); 
+} 
 
-static void my_filter_defaults(obs_data_t *settings)
-{
-	UNUSED_PARAMETER(settings);
-}
+static void my_filter_defaults(obs_data_t *settings) 
+{ 
+	UNUSED_PARAMETER(settings); 
+} 
 
-static obs_properties_t *my_filter_properties(void *data)
-{
-	UNUSED_PARAMETER(data);
+static obs_properties_t *my_filter_properties(void *data) 
+{ 
+	UNUSED_PARAMETER(data); 
 
-	obs_properties_t *props = obs_properties_create();
-	obs_properties_add_text(props, "info", "skip video filter 测试版", OBS_TEXT_INFO);
-	return props;
-}
+	obs_properties_t *props = obs_properties_create(); 
+	obs_properties_add_text(props, "info", "正确渲染版本", OBS_TEXT_INFO); 
+	return props; 
+} 
 
-static void my_filter_update(void *data, obs_data_t *settings)
-{
-	UNUSED_PARAMETER(data);
-	UNUSED_PARAMETER(settings);
-}
+static void my_filter_update(void *data, obs_data_t *settings) 
+{ 
+	UNUSED_PARAMETER(data); 
+	UNUSED_PARAMETER(settings); 
+} 
 
-static void my_filter_video_render(void *data, gs_effect_t *effect)
-{
-	struct my_filter_data *filter = (struct my_filter_data *)data;
+static void my_filter_video_render(void *data, gs_effect_t *effect) 
+{ 
+	struct my_filter_data *filter = (struct my_filter_data *)data; 
 	
-	blog(LOG_INFO, "[YOLO] video_render 被调用！");
+	blog(LOG_INFO, "[YOLO] video_render 开始！"); 
 	
-	obs_source_skip_video_filter(filter->source);
-}
+	// 开始处理滤镜，直接渲染到目标 
+	if (!obs_source_process_filter_begin(filter->source, GS_RGBA, 
+	                                     OBS_ALLOW_DIRECT_RENDERING)) 
+		return; 
+	
+	// 结束处理，使用传入的默认effect（直接透传画面） 
+	obs_source_process_filter_end(filter->source, effect, 0, 0); 
+	
+	blog(LOG_INFO, "[YOLO] video_render 结束！"); 
+} 
 
-struct obs_source_info yolo_filter_info = {
-	.id             = "yolo_recognizer_filter",
-	.type           = OBS_SOURCE_TYPE_FILTER,
-	.output_flags   = OBS_SOURCE_VIDEO,
-	.get_name       = my_filter_name,
-	.create         = my_filter_create,
-	.destroy        = my_filter_destroy,
-	.get_defaults   = my_filter_defaults,
-	.get_properties = my_filter_properties,
-	.update         = my_filter_update,
-	.video_render   = my_filter_video_render,
+struct obs_source_info yolo_filter_info = { 
+	.id             = "yolo_recognizer_filter", 
+	.type           = OBS_SOURCE_TYPE_FILTER, 
+	.output_flags   = OBS_SOURCE_VIDEO, 
+	.get_name       = my_filter_name, 
+	.create         = my_filter_create, 
+	.destroy        = my_filter_destroy, 
+	.get_defaults   = my_filter_defaults, 
+	.get_properties = my_filter_properties, 
+	.update         = my_filter_update, 
+	.video_render   = my_filter_video_render, 
 };
